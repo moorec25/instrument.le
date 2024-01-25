c4d06a87e56dde76ec258dfac83e4e9add2af805
moorec25 <moorec25@mcmaster.ca>
2024-01-24 20:32:01 -0500

    updated environment setup script to work with folder restructuring

--- fpga/envsetup.sh
+++ fpga/envsetup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
-if [ ! -d "$CAPSTONE_HOME/venv" ]; then
+if [ ! -d "$CAPSTONE_HOME/fpga/venv" ]; then
     echo "Creating python venv..."
-    cd $CAPSTONE_HOME
+    cd $CAPSTONE_HOME/fpga
     python3.10 -m venv venv
 fi
 
-source $CAPSTONE_HOME/venv/bin/activate
+source $CAPSTONE_HOME/fpga/venv/bin/activate
 
 echo "Checking requirements"
-pip install -r $CAPSTONE_HOME/requirements.txt | grep "Requirement already satisfied" -v
+pip install -r $CAPSTONE_HOME/fpga/requirements.txt | grep "Requirement already satisfied" -v
 
 export OUT_HOME="$CAPSTONE_HOME/out"
-export TEST_HOME="$CAPSTONE_HOME/DSD100subset/Mixtures"
+export TEST_HOME="$CAPSTONE_HOME/fpga/DSD100subset/Mixtures"
